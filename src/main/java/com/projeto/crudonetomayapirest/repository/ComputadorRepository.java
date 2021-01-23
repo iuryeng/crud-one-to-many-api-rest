5b6612b6f863e0a3eb10187ba4c343ba44f4c79f
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 18:50:03 -0400

    add repositorio de computador

--- src/main/java/com/projeto/crudonetomayapirest/repository/ComputadorRepository.java
+++ src/main/java/com/projeto/crudonetomayapirest/repository/ComputadorRepository.java
@@ -1,2 +1,9 @@
-package com.projeto.crudonetomayapirest.repository;public interface ComputadorRepository {
+package com.projeto.crudonetomayapirest.repository;
+
+import com.projeto.crudonetomayapirest.dominio.Computador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ComputadorRepository extends JpaRepository<Computador, Long> {
 }
