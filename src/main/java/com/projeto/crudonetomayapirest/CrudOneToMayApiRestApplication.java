62e1e59ed8b30573bdac896448532867294723a6
Iury Coelho <38250160+iuryeng@users.noreply.github.com>
2021-01-23 16:54:07 -0400

    add files dos Spring Initializr

---
+++ src/main/java/com/projeto/crudonetomayapirest/CrudOneToMayApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.crudonetomayapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOneToMayApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOneToMayApiRestApplication.class, args);
+	}
+
+}
