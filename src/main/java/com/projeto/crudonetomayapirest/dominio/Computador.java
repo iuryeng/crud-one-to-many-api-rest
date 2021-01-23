5b11369d27fc807507d1a6a9c64444fd964b968f
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 18:37:14 -0400

    add classe computador

--- src/main/java/com/projeto/crudonetomayapirest/dominio/Computador.java
+++ src/main/java/com/projeto/crudonetomayapirest/dominio/Computador.java
@@ -1,2 +1,21 @@
-package com.projeto.crudonetomayapirest.dominio;public class Computador {
+package com.projeto.crudonetomayapirest.dominio;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="Computador")
+public class Computador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String modelo;
+
+    private String marca;
+
+    private Long serviceTag;
+
 }
