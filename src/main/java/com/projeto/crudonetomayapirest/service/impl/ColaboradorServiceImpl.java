f19e6faeac8829b5aa4ccbe67e861329011e458a
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-24 00:11:12 -0400

    add anotation Transactional

--- src/main/java/com/projeto/crudonetomayapirest/service/impl/ColaboradorServiceImpl.java
+++ src/main/java/com/projeto/crudonetomayapirest/service/impl/ColaboradorServiceImpl.java
@@ -8,12 +8,14 @@ import com.projeto.crudonetomayapirest.service.ColaboradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Transactional
 public class ColaboradorServiceImpl implements ColaboradorService {
 
     @Autowired
