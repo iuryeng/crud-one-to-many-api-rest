c5995f26f85ec4335c60b4da482cc16d0729746b
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 21:26:58 -0400

    add implementacao de interface de servico Computador

--- src/main/java/com/projeto/crudonetomayapirest/service/ComputadorServiceImpl.java
+++ src/main/java/com/projeto/crudonetomayapirest/service/ComputadorServiceImpl.java
@@ -1,2 +1,59 @@
-package com.projeto.crudonetomayapirest.service;public class ComputadorServiceImpl {
+package com.projeto.crudonetomayapirest.service;
+
+import com.projeto.crudonetomayapirest.dominio.Computador;
+import com.projeto.crudonetomayapirest.exception.ComputadorNotFoundException;
+import com.projeto.crudonetomayapirest.repository.ComputadorRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class ComputadorServiceImpl implements ComputadorService {
+
+    private ComputadorRepository computadorRepository;
+
+    @Override
+    public Computador createComputador(Computador computador) {
+        return computadorRepository.save(computador);
+    }
+
+    @Override
+    public Computador updateComputador(Long computadorId, Computador computador) {
+        Computador updateComputador = getComputadorById(computadorId);
+        updateComputador.setServiceTag(computador.getServiceTag());
+        updateComputador.setMarca(computador.getMarca());
+        updateComputador.setModelo(computador.getModelo());
+
+        return updateComputador;
+
+    }
+
+    @Override
+    public List<Computador> getAllComputador() {
+        return StreamSupport.stream(computadorRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    @Override
+    public Computador getComputadorById(Long computadorId) {
+        Optional<Computador> computador = computadorRepository.findById(computadorId);
+
+        if(computador.isEmpty()){
+            throw new ComputadorNotFoundException(computadorId);
+        }else{
+            return computador.get();
+        }
+    }
+
+    @Override
+    public void deleteComputador(Long computadorId) {
+        Optional<Computador> computador = computadorRepository.findById(computadorId);
+
+        if(computador.isEmpty()){
+            throw new ComputadorNotFoundException(computadorId);
+        }else{
+            this.computadorRepository.delete(computador.get());
+        }
+
+    }
 }
