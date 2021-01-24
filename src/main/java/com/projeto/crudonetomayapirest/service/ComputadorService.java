3f5d9d3792a4d9fff9288aefcd1d4b0f2e6f069d
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 21:25:42 -0400

    add interface de servico Computador

--- src/main/java/com/projeto/crudonetomayapirest/service/ComputadorService.java
+++ src/main/java/com/projeto/crudonetomayapirest/service/ComputadorService.java
@@ -1,2 +1,19 @@
-package com.projeto.crudonetomayapirest.service;public interface ComputadorService {
+package com.projeto.crudonetomayapirest.service;
+
+import com.projeto.crudonetomayapirest.dominio.Computador;
+
+import java.util.List;
+
+public interface ComputadorService {
+
+    Computador createComputador(Computador computador);
+
+    Computador updateComputador(Long computadorId, Computador computador);
+
+    List<Computador> getAllComputador();
+
+    Computador getComputadorById(Long computadorId);
+
+    void deleteComputador(Long computadorId);
+
 }
