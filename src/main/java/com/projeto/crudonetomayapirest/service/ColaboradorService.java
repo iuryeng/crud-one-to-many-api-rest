dbd45812dbfdaf3af45b847f14abb7b1ad8e501b
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 22:18:04 -0400

    add assinatura de métodos da interface de servico Colaborador

--- src/main/java/com/projeto/crudonetomayapirest/service/ColaboradorService.java
+++ src/main/java/com/projeto/crudonetomayapirest/service/ColaboradorService.java
@@ -1,6 +1,7 @@
 package com.projeto.crudonetomayapirest.service;
 
 import com.projeto.crudonetomayapirest.dominio.Colaborador;
+import com.projeto.crudonetomayapirest.dominio.Computador;
 
 import java.util.List;
 
@@ -8,7 +9,17 @@ public interface ColaboradorService {
 
     Colaborador createColaborador(Colaborador colaborador);
 
-    Colaborador updateColaborador( Colaborador colaborador);
+    Colaborador updateColaborador(Long colaboradorId, Colaborador colaborador);
+
+    List<Colaborador> getAllColaborador();
+
+    Colaborador getColaboradorById(Long colaboradorId);
+
+    Colaborador addComputadorParaColaborador(Long colaboradorId, Long computadorId);
+
+    Colaborador deleteComputadorDeColaborador(Long colaboradorId, Long computadorId);
+
+    void deleteColaborador(Long colaboradorId);
 
 
 }
