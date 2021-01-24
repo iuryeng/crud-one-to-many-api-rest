6c5272b6e17c93bfbbfbf7a4a9a24553b718549c
iuryeng <iuryfernandes.eng@gmail.com>
2021-01-23 21:28:39 -0400

    add excecao NotFound para Computador

--- src/main/java/com/projeto/crudonetomayapirest/exception/ComputadorNotFoundException.java
+++ src/main/java/com/projeto/crudonetomayapirest/exception/ComputadorNotFoundException.java
@@ -1,2 +1,10 @@
-package com.projeto.crudonetomayapirest.exception;public class ComputadorNotFoundException {
+package com.projeto.crudonetomayapirest.exception;
+
+import java.text.MessageFormat;
+
+public class ComputadorNotFoundException extends  RuntimeException{
+
+    public ComputadorNotFoundException(Long id) {
+        super(MessageFormat.format("não é possível econtrar o computador com o id:", id));
+    }
 }
